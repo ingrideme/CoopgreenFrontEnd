45790550a2227bcd18f4e278f28a15a7b1be75fa
unknown <andrade97.fernanda@gmail.com>
2021-06-05 23:33:51 -0300

    Inserção de campos Nome, E-mail

--- src/app/cadastrar/cadastrar.component.ts
+++ src/app/cadastrar/cadastrar.component.ts
@@ -45,7 +45,7 @@ export class CadastrarComponent implements OnInit {
     }else {
       this.authService.cadastrar(this.user).subscribe((resp: User) => {
         this.user = resp
-        this.router.navigate(['/entrar'])
+        this.router.navigate(['/login'])
         alert('Usuário cadastrado com sucesso!')
       })
   }
