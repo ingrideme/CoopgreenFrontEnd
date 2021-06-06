bf969238c76f20e1243505cbc3e46535454352ae
ingrideme <ingrid_melo9@hotmail.com>
2021-06-05 22:01:47 -0300

    Consumo API - Login e Cadastro

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -8,6 +9,8 @@ import { RodapeComponent } from './rodape/rodape.component';
 import { CadastrarComponent } from './cadastrar/cadastrar.component';
 import { LoginComponent } from './login/login.component';
 import { SobreComponent } from './sobre/sobre.component';
+import { SobrenosComponent } from './sobre/sobrenos/sobrenos.component';
+import { FormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -16,11 +19,14 @@ import { SobreComponent } from './sobre/sobre.component';
     RodapeComponent,
     CadastrarComponent,
     LoginComponent,
-    SobreComponent
+    SobreComponent,
+    SobrenosComponent
 ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { UserLogin } from './../model/UserLogin';
+import { AuthService } from './../service/auth.service';
+import { Router } from '@angular/router';
+import { environment } from './../../environments/environment.prod';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +11,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+    userLogin: UserLogin = new UserLogin()
 
-  ngOnInit(): void {
-  }
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) { }
 
+  ngOnInit() {
+    window.scroll(0,0)
+    }
+
+    entrar(){
+      this.authService.entrar(this.userLogin).subscribe((resp: UserLogin) =>
+        {
+          this.userLogin = resp
+
+          environment.token = this.userLogin.token
+
+          this.router.navigate(['/cadastrar'])
+          alert('Logou!')
+        }, erro => {
+          if(erro.status == 500)
+          {
+            alert('Usuário ou senha incorretos')
+          }
+        })
+
+    }
 }
