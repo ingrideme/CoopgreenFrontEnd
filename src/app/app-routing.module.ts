3c2e7a1ffb3421e757c85bf3d1465c9f27cccbb8
ingrideme <ingrid_melo9@hotmail.com>
2021-06-09 12:46:01 -0300

    Cadastrar Produto - Rota Login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,17 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CadastrarComponent } from './cadastrar/cadastrar.component';
 import { LoginComponent } from './login/login.component';
+import { PerfilUsuarioComponent } from './perfil-usuario/perfil-usuario.component';
+
 
 const routes: Routes = [
 
   {path:'', redirectTo:'login', pathMatch:'full'},
 
   {path:'login', component: LoginComponent},
-  {path:'cadastrar', component:CadastrarComponent}
+  {path:'cadastrar', component:CadastrarComponent},
 
+  {path: 'perfil', component: PerfilUsuarioComponent },
 ];
 
 @NgModule({
