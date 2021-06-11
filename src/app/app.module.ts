7a92ddb4f558e5dde668f4a576ca79ce1734dbad
unknown <rocha-igor@hotmail.com>
2021-06-11 09:42:42 -0300

    Atualização de componentes da compra

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,7 @@ import { SobreComponent } from './sobre/sobre.component';
 import { SobrenosComponent } from './sobre/sobrenos/sobrenos.component';
 import { FormsModule } from '@angular/forms';
 import { MenuComponent } from './menu/menu.component';
+import { CompraComponent } from './compra/compra.component';
 
 
 @NgModule({
@@ -22,7 +23,8 @@ import { MenuComponent } from './menu/menu.component';
     LoginComponent,
     SobreComponent,
     SobrenosComponent,
-    MenuComponent
+    MenuComponent,
+    CompraComponent
 ],
   imports: [
     BrowserModule,
